5d2f9c57fd73d16a610723e97e1977c30dfdbf45
Mauro <maurogabriely@gmail.com>
2024-03-18 15:14:07 -0300

    porafolio demo

---
+++ src/App.jsx
@@ -0,0 +1,206 @@
+
+import { NavBar } from './componentes/Navbar'
+import { Footer } from './componentes/Footer'
+
+
+
+
+
+function App() {
+  
+
+  return (
+   
+   <div>
+
+      <div className='burbujas d-flex justify-content-center align-items-center'>
+
+        <span style={{ '--i': 11 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 12 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 24 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 10 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 14 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 23 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 16 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 19 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 20 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 22 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 21 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 11 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 12 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 24 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 10 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 14 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 23 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 16 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 19 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 20 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 22 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 25 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 21 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 21 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 22 }} className='d-inline-block p-3'></span>
+        
+
+        
+
+      </div>
+
+      <div className=''>
+      <div class="container">
+      <h1 className='mb-3'>foto</h1>
+
+      
+      <h1 className='text-white texto-con-sombras-multiples'> 
+        yo soy Mauro 
+        <br></br>
+        <br></br>Full Stack Web Developer
+        </h1>
+
+        <h1 className='mb-3'>redes</h1>
+    </div>
+
+      </div>
+      
+      
+              
+
+        <div className='burbujas d-flex justify-content-center align-items-center'>
+
+        <span style={{ '--i': 11 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 12 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 24 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 10 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 14 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 23 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 16 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 19 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 20 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 22 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 21 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 11 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 12 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 24 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 10 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 14 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 23 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 16 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 19 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 20 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 22 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 25 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 18 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 21 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 21 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 15 }} className='d-inline-block p-3'></span>
+        <span style={{ '--i': 22 }} className='d-inline-block p-3'></span>
+        
+
+        
+
+      </div>
+
+      <div className='box-relative'>
+      <div className='container2'>
+      <section>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+        
+
+        <div className="bracket-container">
+      <div className="left-bracket">
+        <h1>[</h1></div>
+
+      <h2 className="contenedor">
+      <p>tecnologias </p>
+
+      <ul>
+        <li>JS</li>
+        <li>Mongo</li>
+        <li>Css</li>
+      </ul>
+      
+    </h2>
+
+      <div className="right-bracket">
+        <h1>]</h1></div>
+    </div>
+
+                          
+        </section>
+
+       <div>
+
+       <section>
+        <h1>proiec7os</h1>
+        <div className='gallery'>
+        
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsgUwMETk1f4BGQvpVah4GebWHGnruDAh3F9nqmjbSx6IwQkwGZENuLU4f8kHg1hgAbcY&usqp=CAU" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoR3i5uWCbGcMhx84oYS8Ny2Sp_R6dqeJ5MWhT42bDReT6IwXfu_R_c5D6uRZlkLFUifI&usqp=CAU" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsgUwMETk1f4BGQvpVah4GebWHGnruDAh3F9nqmjbSx6IwQkwGZENuLU4f8kHg1hgAbcY&usqp=CAU" alt="" />
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoR3i5uWCbGcMhx84oYS8Ny2Sp_R6dqeJ5MWhT42bDReT6IwXfu_R_c5D6uRZlkLFUifI&usqp=CAU" alt="" />
+          
+                    
+                </div>           
+        </section>
+
+       </div>
+      </div>
+
+      </div>
+      
+      
+      
+      
+      
+      
+      
+      
+          
+      
+
+      
+
+      
+      
+
+      
+      
+          
+      
+                               
+      
+
+        
+       
+
+        <Footer/>
+
+   </div>
+      
+  )
+}
+
+export default App
---
+++ src/componentes/Footer.jsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+    return (
+        <div>
+            <footer>
+                
+                    <p> Todos los derechos reservados. 2023 <b>Mau &copy;</b></p>
+                
+            </footer>
+        </div>
+    )
+} 
